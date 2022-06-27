7a18c6830c124a138729b785d0b07e199e9d77e3
mingyuan <margin.ma@tradplus.com>
2022-06-27 10:09:24 +0800

    test：map

---
+++ map/2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var a = map[int]interface{
+
+	}{}
+
+	//a[4] = map[int]string{2:"123"}
+
+	fmt.Println(reflect.TypeOf(a[4]).String())
+
+	if reflect.TypeOf(a[4]).String() != "string" {
+		a[4] = "123123"
+	}
+
+
+
+	fmt.Println(a[4].(string))
+}
