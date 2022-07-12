3f3ee0500ac04aec218a98ab41eced2302d54828
mingyuan <margin.ma@tradplus.com>
2022-07-12 13:47:42 +0800

    feat：socket

--- socket/client/main.go
+++ socket/client/main.go
@@ -5,6 +5,7 @@ import (
 	"socket/util"
 	"fmt"
 	"net"
+	"time"
 )
 
 func main() {
@@ -16,15 +17,14 @@ func main() {
 	defer conn.Close()
 	fmt.Printf("client %v connected\n", conn.LocalAddr())
 
-	util.Write(conn, "hello111")
-	if _, err := util.Read(conn); err != nil {
-		fmt.Println(err)
-	}
+	for {
+		util.Write(conn, "hello")
+		//如果服务断开，则报错：read tcp 127.0.0.1:59262->127.0.0.1:9090: wsarecv: An existing connection was forcibly closed by the remote host.
+		if _, err := util.Read(conn); err != nil {
+			fmt.Println(err)
+		}
 
-	util.Write(conn, "hello222")
-	if _, err := util.Read(conn); err != nil {
-		fmt.Println(err)
+		time.Sleep(5 * time.Second)
 	}
-
 
 }
